473da88d9cd66880c4832ac71de4d16cd80b811c
Jaggu7292 <103119265+Jaggu7292@users.noreply.github.com>
2022-04-18 15:06:56 +0530

    Add files via upload

---
+++ calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y,z;
+            Console.Write("enter the  first number:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter the second number");
+            y = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("1.Add");
+            Console.WriteLine("2.Sub");
+            Console.WriteLine("3.Mul");
+            Console.WriteLine("4.Div");
+            Console.Write("select the operations which you requried:");
+            z = Convert.ToInt32(Console.ReadLine());
+            switch (z)
+            {
+                case 1:
+                    Console.WriteLine(x+y);
+                    break;
+                case 2: 
+                    Console.WriteLine(x-y);
+                    break;
+                case 3:
+                    Console.WriteLine(x * y);
+                    break;
+                case 4:
+                    Console.WriteLine(x / y);
+                    break;
+                default:
+                    Console.WriteLine("invalid");
+                    break;
+             }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ swaping two numbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapingTwoNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100;
+            int y = 200;
+            
+            int temp = x;
+            x = y;
+            y = temp;
+            Console.WriteLine("swaped the x value "+x);
+            Console.WriteLine("swaped the y value "+y);
+            Console.ReadLine();
+        }
+    }
+}
