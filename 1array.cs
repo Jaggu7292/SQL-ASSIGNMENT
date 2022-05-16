38540a0ccdbc77e19cb2ef78b6a98172176428eb
Jaggu7292 <103119265+Jaggu7292@users.noreply.github.com>
2022-05-16 11:32:35 +0000

    Add files via upload

---
+++ 1array.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System;
+namespace sample
+{
+    class array
+    {
+        static void Main(string[] args)
+        {
+            // Creating an array  
+            int[] arr = new int[6] { 4,6,24,27,3,8 };
+             
+            int[] arr2 = new int[6];
+            
+            Console.WriteLine("length of first array: " + arr.Length);
+           
+            Array.Sort(arr);
+            Console.Write("First array elements: ");
+           
+            PrintArray(arr);
+              
+            Console.WriteLine("\nIndex position of 27 is " + Array.IndexOf(arr, 27));
+
+           
+            Array.Copy(arr, arr2, arr.Length);
+            Console.Write("Second array elements: ");
+          
+
+  
+            PrintArray(arr2);
+            Array.Reverse(arr);
+            Console.Write("\nFirst Array elements in reverse order: ");
+            PrintArray(arr);
+        }
+         
+        static void PrintArray(int[] arr)
+        {
+            foreach (Object elem in arr)
+            {
+                Console.Write(elem + " ");
+            }
+        }
+    }
+}
---
+++ 3addemployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeedata
+{
+    internal class addemployee
+    {
+        static void Main()
+        {
+            List<string> list = new List<string>();
+            list.Add("jaggu");
+            list.Add("Ranjith");
+            list.Add("Kumar");
+            list.Add("pandy");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine("List of employees" + ':' + list[i]);
+            }
+            Console.WriteLine("total number of employess" + ':' + list.Count);
+            Console.ReadLine();
+        }
+    }
+}
